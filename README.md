3b3d1b7777ac4c5f2b87695262c424274133b80e
thmijo <thmijomail@gmail.com>
2019-06-14 11:23:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# examin-0614
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/examin-0614)
---
+++ src/app/exam/exam.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {ExamService} from '../shared/exam.service';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-exam',
+  templateUrl: './exam.component.html',
+  styleUrls: ['./exam.component.css']
+})
+export class ExamComponent implements OnInit {
+exams: any = [];
+tempQId : string;
+
+  constructor(private examService : ExamService,private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    console.log("Getting Questions");
+    // console.log(this.questionService.getQuestions());
+
+    
+    this.examService.getExams().subscribe(exams => {
+      this.exams = exams;
+     // console.log(this.exams);
+    });
+
+  }
+
+  addExam() {
+    console.log("adding dummy exam");
+    this.examService.addExam();
+    //this.examService.
+  }
+
+  deleteExam(eId:string) {
+    console.log("Deleting Exam"+eId);
+    this.examService.deleteExam(eId);
+  }
+ loadQuestions (eId:string) {
+      console.log ("Loading questions noww.sdasd 9999. "+eId);
+      this.examService.loadQuestions(eId);
+      //console.log ("Loading questions noww.. "+message);
+      this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe((res)=>{
+            console.log ("Tying it");
+            console.log(res);
+        });
+  }
+
+}
